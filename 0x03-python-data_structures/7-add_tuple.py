631bd116439c0053851096defc8450dc770dfdaa
owen20108 <owen20108@yahoo.com>
2023-10-07 16:46:02 -0700

    data strcutre

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-   
-    a = tuple_a[:2] + (0, 0)
-    b = tuple_b[:2] + (0, 0)
-    
-    
-    rslt_tple = (a[0] + b[0], a[1] + b[1])
-    
-    return rslt_tple
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
+    return(tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
