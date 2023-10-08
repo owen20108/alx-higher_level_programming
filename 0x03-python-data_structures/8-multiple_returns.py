ee556cd833ecdf465b8cafcf730c8217b3b0f9cb
owen20108 <owen20108@yahoo.com>
2023-10-08 15:41:18 -0700

    data

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    strlnght = len(sentence)
-    char = sentence[0] if strlnght > 0 else "None"
-    new = strlnght, char
-    return(chstrlnght)
+    if sentence == "":
+        return (0, None)
+    return (len(sentence), sentence[0])
