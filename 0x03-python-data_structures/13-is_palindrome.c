52118522fa66b805cab7d731a6e50eafceb7dac3
owen20108 <owen20108@yahoo.com>
2023-10-09 15:40:13 -0700

    palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,53 +1,40 @@
 #include "lists.h"
-/**
- * reverse_list - reverse  list
- * @head: pointer to list
- * Return: add of new head
- */
-listint_t *reverse_list(listint_t **head)
-{
-	listint_t *prev = NULL, *next;
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-	while (*head != NULL)
-	{
-		next = (*head)->next;
-		(*head)->next = prev;
-		prev = *head;
-		*head = next;
-	}
-
-	*head = prev;
-	return (*head);
-}
-/**
- * is_palindrome - tests if list is a palindrome
- * @head: pointer to list
- * Return: 1 if palindrome, 0 if not
- */
+/** * 
+is_palindrome - tests if list is a palindrome
+* @head: pointer to list
+* Return: 1 if palindrome, 0 if not
+*/
 int is_palindrome(listint_t **head)
 {
-	listint_t *slower = *head, *faster = *head, *one, *two;
+	listint_t *tmpo = *head;
+	int nodez = 0, i = 0, *array = NULL;
 
-	if (!(head && *head) || (*head)->next == NULL)
+	if (*head == NULL || head == NULL || (*head)->next == NULL)
 		return (1);
-	while (faster != NULL && faster->next != NULL)
+	while (tmpo)
 	{
-		faster = faster->next->next;
-		one = slower;
-		slower = slower->next;
+		nodez++;
+		tmpo = tmpo->next;
 	}
-
-	slower = reverse_list(&slower);
-	two = slower;
-	faster = *head;
-	while (faster && slower)
+	array = malloc(sizeof(int) * nodez);
+	tmpo = *head;
+	while (tmpo)
 	{
-		if (faster->n != slower->n)
+		array[i++] = tmpo->n;
+		tmpo = tmpo->next;
+	}
+	for (i = 0; i < nodez / 2; i++)
+	{
+		if (array[i] != array[nodez - 1 - i])
+		{
+			free(array);
 			return (0);
-		faster = faster->next;
-		slower = slower->next;
+		}
 	}
-	slower = reverse_list(&two);
-	one->next = slower;
+	free(array);
 	return (1);
 }
