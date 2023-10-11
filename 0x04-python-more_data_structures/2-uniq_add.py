ceb561f9eff757c35a6b5a24d426d3aaa19799cb
owen20108 <owen20108@yahoo.com>
2023-10-11 14:08:21 -0700

    matrix

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    copylist = []
+    for it in my_list:
+        if it not in copylist:
+            copylist.append(i)
+    reslt = 0
+    for it in copylist:
+        reslt = reslt + it
+    return reslt
