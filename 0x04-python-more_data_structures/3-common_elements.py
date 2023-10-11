7913c0fb7b61d4a47aac991d6a7f67691bd9ff3b
owen20108 <owen20108@yahoo.com>
2023-10-11 14:17:56 -0700

    matrix

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,8 @@
+!/usr/bin/python3
+def common_elements(set_1, set_2):
+    c_set = []
+    for it in set_1:
+        for jt in set_2:
+            if it == jt:
+                c_set.append(it)
+    return c_set
