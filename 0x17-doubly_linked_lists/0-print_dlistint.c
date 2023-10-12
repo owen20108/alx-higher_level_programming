d10ca6520b8fbc329a91543931dcb8a66e90537d
owen20108 <owen20108@yahoo.com>
2023-10-12 15:16:28 -0700

    linked list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,15 +10,15 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	const dlistint_t *pntr = h;
-	size_t no = 0;
+	size_t n = 0;
 
 	while (pntr && pntr->prev)
 		pntr = pntr->prev;
 	while (pntr)
 	{
-		printf("%d\n", pntr->no);
-		no++;
+		printf("%d\n", pntr->n);
+		n++;
 		pntr = pntr->next;
 	}
-	return (no);
+	return (n);
 }
