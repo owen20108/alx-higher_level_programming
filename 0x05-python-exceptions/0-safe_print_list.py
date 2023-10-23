92136a0591001b5ca3d95666ca79410353dffcb8
owen20108 <owen20108@yahoo.com>
2023-10-23 13:35:16 -0700

    safe

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    printed_count = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            printed_count += 1
+        except IndexError:
+            break
+    print()
+    return printed_count
+
