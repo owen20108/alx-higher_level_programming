f3016f2453c7b1594a57e5a3eb584c663eda10cd
owen20108 <owen20108@yahoo.com>
2023-10-23 13:53:16 -0700

    safe

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    var = 0
+    for va in range(x):
+        try:
+            print("{}".format(my_list[va]), end="")
+            var += 1
+        except IndexError:
+            break
+    print("")
+    return(var)
