24bcb9a34c5b8216da1a32cf3550248f81b85bc9
owen20108 <owen20108@yahoo.com>
2023-10-24 15:06:49 -0700

    square

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
+"""writing a docstring"""
 import math
 
 
 class MagicClass:
-    """ class that create circle area"""
+    """set up the magic"""
 
     def __init__(self, radius=0):
-        """ initializing """
+        """ writing another docstring """
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
         self.__radius = radius
 
     def area(self):
-        """ function defining area """
-        return (self.__radius ** 2 * math.pi)
+        """again with the docstring"""
+        return self.__radius ** 2 * math.pi
 
     def circumference(self):
-        """ function defining circuference """
-        return ((2 * math.pi) * self.__radius)
+        """such docstring"""
+        return 2 * math.pi * self.__radius
