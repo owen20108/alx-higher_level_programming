acece2bd1a59604feef6d527278892a976b199d9
owen20108 <owen20108@yahoo.com>
2023-10-24 13:41:31 -0700

    square

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -13,6 +13,25 @@ class Square:
 
     """
 
+    @property
+    def size(self):
+        """int: length of square sides
+
+        The setter validates that the size is an integer and is 0 or greater
+
+        """
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
     def __init__(self, size=0):
         """Creates a square of a given size
 
