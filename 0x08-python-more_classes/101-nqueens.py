baec6807c61921f17a056b066ef395b593d62c8f
owen20108 <owen20108@yahoo.com>
2023-10-31 06:27:35 -0700

    edit

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     a = []
     if len(argv) != 2:
         print("Usage: nqueens N")
-        exit(1)
+        exit (1)
     if argv[1].isdigit() is False:
         print("N must be a number")
         exit(1)
