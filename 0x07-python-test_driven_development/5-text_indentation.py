730aa7c9d27f9464e52abad5ce554daef82401d9
owen20108 <owen20108@yahoo.com>
2023-11-01 15:18:10 -0700

    test

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,15 +1,36 @@
 #!/usr/bin/python3
 """
-This module prints text with 2 newlines after
-character - . ? :
+
+This module contains a function that indents texts
+
 """
 
 
 def text_indentation(text):
-    """
-    function to print text and 2 newlines
-    """
-    if text is None or not isinstance(text, str) or len(text) < 0:
-        raise TypeError('text must be a string')
-    string = "".join([a if a not in ".?:" else a + "\n\n" for a in text])
-    print("\n".join([x.strip() for x in string.split("\n")]), end="")
+    '''This function prints a text with 2 new lines after each ".", "?", or ":"
+
+    Args:
+        text (str): The string to be printed
+
+    Raises:
+        TypeError: If text is not a string
+
+    '''
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    count = 0
+    while count < len(text) and text[count] == " ":
+        count = count + 1
+
+    while count < len(text):
+        print(text[count], end="")
+        if text[count] == "\n" or text[count] in ".?:":
+            if text[count] in ".?:":
+                print("\n")
+            count = count + 1
+            while count < len(text) and text[count] == " ":
+                count = count + 1
+            continue
+        count = count + 1
