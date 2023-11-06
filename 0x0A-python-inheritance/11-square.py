805cdbeb6c16c562ecb2944ef9d852ed66790b21
owen20108 <owen20108@yahoo.com>
2023-11-06 15:52:56 -0800

    inhert

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,77 +1,30 @@
 #!/usr/bin/python3
 """
-This module contains a class
-with public instance and Raises
-exception when required
+    Square Module
 """
-
-
-class BaseGeometry:
-    """
-    class Base has 2 public instances
-    """
-    def area(self):
-        """
-        function that raises exception
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        function that validates value
-        """
-        if not isinstance(value, int):
-            raise TypeError("{:s} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{:s} must be greater than 0".format(name))
-
-
-class Rectangle(BaseGeometry):
-    """
-    class Rectangle with private height and width
-    """
-    def __init__(self, width, height):
-        """
-        instantiation of class
-        """
-        self.integer_validator("width", width)
-        self.__width = width
-        self.integer_validator("height", height)
-        self.__height = height
-
-    def area(self):
-        """
-        returns area of a rectangle
-        """
-        return (self.__width * self.__height)
-
-    def __str__(self):
-        """
-        string representation of the rectangle
-        """
-        return("[Rectangle] {:d}/{:d}".format(self.__width, self.__height))
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
     """
-    class Square that inherits from rectangle
+        Square Class
     """
     def __init__(self, size):
         """
-        class instantiation
+            Initialize the square base on Rectangle
         """
-        self.integer_validator("size", size)
+        self.integer_validator('size', size)
         self.__size = size
         super().__init__(size, size)
 
     def area(self):
         """
-        function returns area of square
+        define area of the square
         """
-        return (self.__size ** 2)
+        return self.__size ** 2
 
     def __str__(self):
         """
-        string representation of the square
+            __str__
         """
-        return("[Square] {:d}/{:d}".format(self.__size, self.__size))
+        return ("[Square] {:d}/{:d}".format(self.__size, self.__size))
