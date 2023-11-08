b737f3c3fd8f1684b5102844d416bc7b5615213c
owen20108 <owen20108@yahoo.com>
2023-11-08 12:59:34 -0800

    in out

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python3
-"""
-A Student class that defines a Studen (module)
-"""
+"""This module defines a class Student"""
 
 
-class Student():
-    """
-        A Student class that defines a Student
-    """
+class Student:
+    """Represent a student."""
+
     def __init__(self, first_name, last_name, age):
-        """
-            INIT
+        """Initializes a new Student
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
+        """Get a dictionary representation of the Student
+        If attrs is a list of strings, represents only those attributes
+        included in the list
         """
-            retrieves a dictionary representation
-            of a Student instance
-        """
-        if type(attrs) is list and all([type(x) == str for x in attrs]):
-            return {k: v for k, v in self.__dict__.items() if k in attrs}
-        return(self.__dict__)
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
