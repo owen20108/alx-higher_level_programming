8fa098d2b3aa90f848950df2ef6a4f374c096886
owen20108 <owen20108@yahoo.com>
2023-11-08 12:52:00 -0800

    in out

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,24 +1,17 @@
 #!/usr/bin/python3
-"""
-A Student class that defines a Studen (module)
-"""
+"""This module defines a class Student"""
 
 
-class Student():
-    """
-        A Student class that defines a Student
-    """
+class Student:
+    """Represent a student."""
+
     def __init__(self, first_name, last_name, age):
-        """
-            INIT
+        """Initializes a new Student
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """
-            retrieves a dictionary representation
-            of a Student instance
-        """
-        return(self.__dict__)
+        """Gets a dictionary representation of the Student"""
+        return self.__dict__
