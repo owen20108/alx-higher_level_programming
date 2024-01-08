87457358615224f798b0fb52363f4d27f05dfa5e
owen20108 <owen20108@yahoo.com>
2024-01-08 10:16:29 -0800

    js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-if (process.argv[3]) {
-  console.log('Arguments found');
-} else if (process.argv[2]) {
-  console.log('Argument found');
+const argc = process.argv.length;
+
+if (argc > 2) {
+  console.log('Argument' + (argc > 3 ? 's' : '') + ' found');
 } else {
   console.log('No argument');
 }
