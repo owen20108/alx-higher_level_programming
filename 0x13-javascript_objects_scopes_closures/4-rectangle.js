8ff99e6208e35f01c29f6bee4fc85f8b3fec6194
owen20108 <owen20108@yahoo.com>
2024-01-09 07:29:20 -0800

    js 2

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,56 +1,33 @@
 #!/usr/bin/node
-/**
- * Represents a rectangle class with width and height attributes.
- * Validates parameters during instantiation.
- */
+
 class Rectangle {
-  /**
-   * Constructor for the Rectangle class.
-   * @param {number} w - The width of the rectangle (must be a positive number).
-   * @param {number} h - The height of the rectangle (must be a positive number).
-   */
-  constructor(w, h) {
-    // Validate parameters
-    if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
+  constructor (w, h) {
+    // Create an empty object if width and/or height is an invalid number.
+    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) { return; }
+    this.width = w;
+    this.height = h;
   }
 
-  /**
-   * Prints a visual representation of the rectangle using 'X' characters.
-   */
-  print() {
+  print () {
     for (let i = 0; i < this.height; i++) {
-      let row = '';
-      let y = 0;
-      while (y < this.width) {
-        row += 'X';
-        y++;
+      let square = '';
+      for (let j = 0; j < this.width; j++) {
+        square += 'X';
       }
-
-      console.log(row);
+      console.log(square);
     }
   }
 
-  /**
-   * Rotates the rectangle by swapping its width and height.
-   */
-  rotate() {
-    let temp = 0;
-    temp = this.width;
+  rotate () {
+    const placeholder = this.width;
     this.width = this.height;
-    this.height = temp;
+    this.height = placeholder;
   }
 
-  /**
-   * Doubles the dimensions (width and height) of the rectangle.
-   */
-  double() {
+  double () {
     this.width *= 2;
     this.height *= 2;
   }
 }
 
 module.exports = Rectangle;
-
