455b3ef7622b343390bca48bf1a8a216d5a9293d
owen20108 <owen20108@yahoo.com>
2024-01-09 07:25:44 -0800

    js 2

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,18 +1,10 @@
 #!/usr/bin/node
-/**
- * Represents a rectangle class with width and height attributes.
- */
+
 class Rectangle {
-  /**
-   * Constructor for the Rectangle class.
-   * @param {number} w - The width of the rectangle.
-   * @param {number} h - The height of the rectangle.
-   */
-  constructor(w, h) {
+  constructor (w, h) {
     this.width = w;
     this.height = h;
   }
 }
 
 module.exports = Rectangle;
-
