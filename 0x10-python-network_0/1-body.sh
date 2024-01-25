f26d41ccce5068b0a5bfe2f5c59e59abd003e4d3
owen20108 <owen20108@yahoo.com>
2024-01-25 12:11:45 -0800

    python

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# send a request to an URL with curl, and displays the size of the body of the response
-curl -s "$1" | wc -c
+# script that takes a url and sends a GET request
+curl -sLf "$1"
