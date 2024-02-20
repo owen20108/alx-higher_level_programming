0627ff7f2ee25f8dfb5d0b1aad442f65a98bcc69
owen20108 <owen20108@yahoo.com>
2024-02-20 15:22:08 -0800

    web

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
+// Read from file
 
-const fs = require('fs');
-const filename = process.argv[2];
-
-fs.readFile(filename, 'utf-8', (error, content) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(content);
-  }
-});
+const filesys = require('fs');
+filesys.readFile(process.argv[2], 'utf-8',
+  function (err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(data);
+  });
